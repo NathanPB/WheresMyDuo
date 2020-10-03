c51f79dd223d87f388ef3c50cb56c2134b61a2ac
NathanPB <nathan.pbombana@gmail.com>
2020-10-03 19:19:54 -0300

    Setting up controller workspace

---
+++ controller/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.4.10"
+}
+
+group = "dev.nathanpb"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
